6e13939b816fe43eb60b44bc80d9a2fad8fc8534
Magesh <tkmagesh77@gmail.com>
2018-06-19 16:09:20 +0530

    greeter part 1

---
+++ first-app/src/app/greeter/greeter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector : 'app-greeter',
+	templateUrl : 'greeter.component.html',
+	styleUrls : ['greeter.component.css']
+})
+export class GreeterComponent{
+	greetMessage = '[A sample greet message]';
+
+	onGreetClick(){
+		let userName = prompt('Enter the user name :');
+		this.greetMessage = `Hi ${userName}, Have a nice day!`;
+	}
+}
