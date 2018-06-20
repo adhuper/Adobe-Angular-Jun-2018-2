62e088199550af8e09b34f4f0379807776547020
Magesh <tkmagesh77@gmail.com>
2018-06-20 16:32:46 +0530

    refactoring non-ui into service (bugOperations)

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 
+import { Bug } from './models/Bug';
+import { BugOperationsService } from './services/bugOperations.service';
+
+
 @Component({
 	selector : 'app-bug-tracker',
 	templateUrl : 'bugTracker.component.html'
 })
 export class BugTrackerComponent{
 
-	bugs : string[] = [];
+	bugs : Bug[] = [];
+
+	/*bugOperations : BugOperationsService = null;
+
+	constructor(_bugOperations : BugOperationsService){
+		this.bugOperations = _bugOperations;
+	}*/
+
+	constructor(private bugOperations : BugOperationsService){
+		
+	}
 
 	onAddNewClick(bugName){
-		this.bugs.push(bugName);
-		console.table(this.bugs);
+		let newBug : Bug = this.bugOperations.createNew(bugName);
+		this.bugs.push(newBug);
+	}
+
+	onBugNameClick(bug){
+		this.bugOperations.toggle(bug);
+	}
+
+	onRemoveClosedClick(){
+		this.bugs = this.bugs.filter(bug => !bug.isClosed);
+	}
+
+	getClosedCount(){
+		return this.bugs.reduce((result, bug) => bug.isClosed ? ++result : result , 0);
 	}
 }
