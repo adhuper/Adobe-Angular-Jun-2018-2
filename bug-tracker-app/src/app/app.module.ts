6df42563dd32a3e3fb146125cf6b954ab6db24c9
Magesh <tkmagesh77@gmail.com>
2018-06-21 16:31:14 +0530

    refactoring to utils module

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { UtilsModule } from './utils/utils.module';
+
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 import { BugStatsComponent } from './bugTracker/views/bugStats.component';
 
 import { BugOperationsService } from './bugTracker/services/bugOperations.service';
 import { BugStorageService } from './bugTracker/services/bugStorage.service';
-
-import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
-import { SortPipe } from './bugTracker/pipes/sort.pipe';
 
 import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
 
@@ -18,12 +17,11 @@ import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
     AppComponent,
     BugTrackerComponent,
     BugStatsComponent,
-    ClosedCountPipe,
-    TrimTextPipe,
-    SortPipe
+    ClosedCountPipe
   ],
   imports: [
     BrowserModule
+    , UtilsModule
   ],
   providers: [
   	BugOperationsService
